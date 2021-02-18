e5a8ab765f4a5bb086989067c9a361e9bd4a7b02
Adam C <45860081+AdamCader00@users.noreply.github.com>
2021-02-17 23:04:50 -0800

    Update strat.sol

--- strat.sol
+++ strat.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.6.11;
+pragma experimental ABIEncoderV2;
+
+import "deps/@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "deps/@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "deps/@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "deps/@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
+import "interfaces/aave/IUniswapRouterV2.sol";
+import "interfaces/badger/IBadgerGeyser.sol";
+
+import "interfaces/sushi/ISushiChef.sol";
+import "interfaces/sushi/IxSushi.sol";
+
+import "interfaces/badger/IController.sol";
+import "interfaces/badger/IMintr.sol";
+import "interfaces/badger/IStrategy.sol";
+
+import "../BaseStrategySwapper.sol";
